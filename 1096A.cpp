20cb4a3526a2e9a59e05355b32c9259d2eb18448
Mahfuz Ahmed <likhon.codew@gmail.com>
2018-12-31 17:20:27 +0600

    Solved 1096A-Find Divisible

---
+++ 1096A.cpp
@@ -0,0 +1,36 @@
+//
+//  1096A.cpp
+//  
+//
+//  Created by Mahfuz Ahmed on 12/31/18.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define br std::cout << "\n";
+#define push push_back
+
+const double pi = 2*acos(0.0);
+const long long mod = 1e9+7;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #if 0
+    freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    #endif
+    
+    int t;
+    cin>>t;
+    while(t--){
+        lli l,r;
+        cin>>l>>r;
+        cout<<l<<" "<<l*2;br;
+    }
+    
+    return 0;
+}
