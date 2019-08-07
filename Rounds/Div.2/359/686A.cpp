032084fb0640351a1d6a911c4c2896b8b425e578
Mahfuz001 <likhon.codew@gmail.com>
2019-08-07 13:47:04 +0600

    solved 686A-Free Ice Cream

---
+++ Rounds/Div.2/359/686A.cpp
@@ -0,0 +1,51 @@
+/**
+ * 	author: MahfuzAhmed
+ * 	created: 07.08.2019 
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define forn(var,from,to) for(int var=(from);var<(to);var++)
+#define forx(var,from,to) for(int var=(from);var<=(to);var++)
+#define fory(var,from,to) for(int var=(from);var>=(to);var--)
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vvi vector<vi>
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define all(p) p.begin(),p.end()
+typedef long long ll;
+//~ const double pi = 2*acos(0.0);
+//~ const long long mod = 1e9+7;
+//~ const int MAX_N = 1e6+5;
+
+int main(int argc, char* argv[]){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+       
+    #ifdef _DEBUG
+    freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    #endif
+    
+    ll n, x; cin >> n >> x;
+    ll d = 0;
+    
+    while(n--){
+        char c; ll p; cin >> c >> p;
+        
+        if( c == '+') x += p;
+        else if(x >= p) x -= p;
+        else d++;
+    }
+    
+    cout << x  << " " << d << "\n";
+    
+    
+    return 0;
+}
+
